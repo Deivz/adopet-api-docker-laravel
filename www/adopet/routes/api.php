98656406e0074898d331c1e4f32b77972db861b4
Deivz <carlosd_oliveira@hotmail.com>
2025-07-21 22:52:54 -0300

    Rota de usuários implementada

--- www/adopet/routes/api.php
+++ www/adopet/routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::controller(UsuarioController::class)->group(function() {
+    Route::get('/users', 'index');
+    Route::get('/users/{user}', 'show');
+    Route::post('/users', 'store');
+    Route::put('/users/{user}', 'update');
+    Route::delete('/users/{user}', 'destroy');
 });
