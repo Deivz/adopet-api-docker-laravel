ab08638bde14a3f8f6092703ef464a6eaff0fe47
Deivz <carlosd_oliveira@hotmail.com>
2025-07-22 22:06:48 -0300

    Alterações diversas

--- www/adopet/routes/api.php
+++ www/adopet/routes/api.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::controller(UsuarioController::class)->group(function() {
     Route::get('/users', 'index');
     Route::get('/users/{user}', 'show');
     Route::post('/users', 'store');
     Route::put('/users/{user}', 'update');
     Route::delete('/users/{user}', 'destroy');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
