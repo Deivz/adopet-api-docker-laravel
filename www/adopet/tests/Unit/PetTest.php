994a3c007c4f828e89ca771035359b1168767f23
Deivz <carlosd_oliveira@hotmail.com>
2025-07-31 22:44:31 -0300

    Alteração nos testes do Pet

--- www/adopet/tests/Unit/PetTest.php
+++ www/adopet/tests/Unit/PetTest.php
@@ -26,26 +26,4 @@ class PetTest extends TestCase
 
     $this->assertEquals(0, count($arrayComparison));
   }
-
-  // public function test_if_columns_from_pets_are_correct(): void
-  // {
-  //     $pet = Pet::make([
-  //         'name' => 'Dunga',
-  //         'age' => '2 anos',
-  //         'size' => 'Pequeno',
-  //         'temperament' => 'Alegre e esperto',
-  //         'city' => 'Salvador',
-  //         'country' => 'Bahia',
-  //         'photo' => 'zxzxzx.png',
-  //         'cod_responsible' => 2,
-  //         'cod_adopter' => 2
-  //     ]);
-  //     $expected = [
-  //         'name', 'age', 'size', 'temperament', 'city', 'country', 'photo', 'cod_responsible', 'cod_adopter'
-  //     ];
-
-  //     $arrayComparison = array_diff($expected, $pet->getFillable());
-
-  //     $this->assertEquals(0, count($arrayComparison));
-  // }
 }
